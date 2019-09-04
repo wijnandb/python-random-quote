f202f47652b13e4bec7132fe596c3a566723a972
Wijnand Baretta <wijnandb@retta.nl>
2019-09-04 19:14:47 +0200

    randomize added

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,14 @@
+import random
+
 def primary():
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes[13])
+  last = len(quotes)-1
+  rnd = random.randint(0,last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
